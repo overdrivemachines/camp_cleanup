4ce7cfb6aefb95cab6f18dc45745ae938d929a14
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-09 21:39:13 -0800

    reading input

--- camp_cleanup.cpp
+++ camp_cleanup.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+  string line;
+  while (!cin.eof()) {
+    cin >> line;
+
+    cout << line << endl;
+
+    cin.get();   // reads in the new line character \n after the input (on the same line)
+    cin.peek();  // reads first character of the next line
+  }
+
   return 0;
 }
